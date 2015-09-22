e141949a9167fc757583b7a2146a1c0c08c62c40
Andre Herman <amelco.herman@gmail.com>
2015-09-22 15:25:12 -0300

    trying to fix the undefined error. Converted utf8 files to latin 1

--- make.sh
+++ make.sh
@@ -14,8 +14,8 @@ pdftex -fmt=${hifen}.fmt ${file}
 bibtex ${file}
 pdftex -fmt=${hifen}.fmt ${file}
 pdftex -fmt=${hifen}.fmt ${file}
-rm ${hifen}.fmt ${hifen}.log ${file}.blg
-rm ${file}.aux ${file}.ref ${file}.bbl
+#rm ${hifen}.fmt ${hifen}.log ${file}.blg
+#rm ${file}.aux ${file}.ref ${file}.bbl
 #rm ${file}.log 
 
 exit 0
