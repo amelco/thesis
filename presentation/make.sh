5b3b48dca85a94d31b522d1768743afd411692b6
Andre Herman Freire Bezerra <amelco.herman@gmail.com>
2016-01-08 16:35:47 -0300

    Modified make.sh to remove all aux files

--- presentation/make.sh
+++ presentation/make.sh
@@ -2,9 +2,15 @@
 fileN=thesis_pres
 pdflatex $fileN.tex
 pdflatex $fileN.tex
-rm $fileN.aux
-rm $fileN.log
-rm $fileN.nav
-rm $fileN.out
-rm $fileN.snm
-rm $fileN.toc
+#rm $fileN.aux
+#rm $fileN.log
+#rm $fileN.nav
+#rm $fileN.out
+#rm $fileN.snm
+#rm $fileN.toc
+rm *.aux
+rm *.log
+rm *.nav
+rm *.out
+rm *.snm
+rm *.toc
